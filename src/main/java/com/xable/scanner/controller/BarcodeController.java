7798f1cae4706e5ca94e882e355b585c6c988a22
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-01 23:31:26 +0530

    barcode scanner uploaded

--- src/main/java/com/xable/scanner/controller/BarcodeController.java
+++ src/main/java/com/xable/scanner/controller/BarcodeController.java
@@ -1,6 +1,5 @@
 package com.xable.scanner.controller;
 
-
 import com.xable.scanner.model.BarcodeData;
 import com.xable.scanner.repository.BarcodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/barcodes")
-@CrossOrigin(origins = "*")
-// Use your React dev server port
+@CrossOrigin // <--- Allows all origins
 public class BarcodeController {
 
     @Autowired
