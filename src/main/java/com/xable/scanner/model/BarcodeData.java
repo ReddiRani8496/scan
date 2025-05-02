2a578c51e5ba1f15a27cbe919f67907f1b45825d
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-02 21:13:44 +0530

    barcode scanner uploaded

--- src/main/java/com/xable/scanner/model/BarcodeData.java
+++ src/main/java/com/xable/scanner/model/BarcodeData.java
@@ -1,6 +1,5 @@
 package com.xable.scanner.model;
 
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,15 +8,55 @@ public class BarcodeData {
     @Id
     private String id;
     private String code;
+    private String productName;
+    private double mrp;
+    private double discount;
+    private String weight;
 
-    // Constructors
     public BarcodeData() {}
-    public BarcodeData(String code) { this.code = code; }
 
-    // Getters and Setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getCode() { return code; }
-    public void setCode(String code) { this.code = code; }
+    public BarcodeData(String code, String productName, double mrp, double discount, String weight) {
+        this.code = code;
+        this.productName = productName;
+        this.mrp = mrp;
+        this.discount = discount;
+        this.weight = weight;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public double getMrp() {
+        return mrp;
+    }
+    public void setMrp(double mrp) {
+        this.mrp = mrp;
+    }
+    public double getDiscount() {
+        return discount;
+    }
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+    public String getWeight() {
+        return weight;
+    }
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+    
 }
-
